db2c7edbefb441d513032964b5810563e638d422
lightleo7 <erovlev2@yandex.ru>
2026-06-01 15:02:57 +0300

    second rework

--- server/frontend/static/main.js
+++ server/frontend/static/main.js
@@ -1,9 +1,5 @@
-// src/main.js
-// import './style.css';
-
 // frontend/static/main.js
-
-// Импортируем конкретные файлы компонентов, как они лежат в пакете npm
 import '@material/web/button/filled-button.js';
 import '@material/web/button/text-button.js';
-import '@material/web/icon/icon.js';
+import '@material/web/iconbutton/icon-button.js';
+import '@material/web/fab/fab.js';
