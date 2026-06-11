9205773d364a929d5f02e805e5b7b736f04fa354
Oedada <Oedada>
2026-06-12 00:07:20 +0300

    rechange Fbuf to non-blocking without async

--- client/src/client/capture/frame_buffer.py
+++ client/src/client/capture/frame_buffer.py
@@ -1,22 +1,23 @@
-import asyncio
+from queue import Queue, Full, Empty
 
 class FBuf:
+
     def __init__(self, val_type: type, maxsize: int = 3):
         self.val_type: type = val_type
         self._maxsize: int = maxsize
-        self.q = asyncio.Queue(maxsize=self._maxsize)
+        self.q = Queue(maxsize=self._maxsize)
 
-    async def put(self, value):
+    def put(self, value):
         if not isinstance(value, self.val_type):
-            raise TypeError(f"В очередь типа {self.val_type.__name__} нельзя положить элемент с типом {type(value).__name__}")
-        
-        if self.q.full():
-            try:
-                self.q.get_nowait()
-            except asyncio.QueueEmpty:
-                pass
-        
-        await self.q.put(value)
+            raise TypeError(f"В очередь типа {self.val_type.__name__} нельзя положить элемент с типом  {type(value).__name__}")
+        try:
+            self.q.put_nowait(value)
+        except Full:
+            self.q.get_nowait()
+            self.q.put_nowait(value)
 
-    async def get(self):
-        return await self.q.get()
+    def get(self):
+        try:
+            return self.q.get()
+        except Empty:
+            return None
